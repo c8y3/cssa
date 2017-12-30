093e94716863cf0e9f7f7beccbbb428c608871e5
c8y3 <25362953+c8y3@users.noreply.github.com>
2017-12-30 18:24:38 +0100

    [add] Collect all values associated to each property

--- src/PropertyValues.js
+++ src/PropertyValues.js
@@ -1,15 +1,20 @@
 export default function() {
 
-    var values = new Set();
+    var values = {};
 
     var self = {};
 
-    self.add = function(property) {
-        values.add(property);
+    self.add = function(property, value) {
+        var previous = values[property];
+        if (previous === undefined) {
+            previous = new Set();
+            values[property] = previous;
+        }
+        previous.add(value);
     };
 
     self.toArray = function() {
-        return Array.from(values);
+        return Object.keys(values);
     };
 
     return self;
