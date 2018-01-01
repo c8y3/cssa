33c1ce26fda8da7654ede195c8fd25f6eb3c6d7c
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-01-01 12:10:41 +0100

    [clean] Moved class ReportWriter out

--- src/Program.js
+++ src/Program.js
@@ -1,8 +1,7 @@
 import FileOperations from '/FileOperations';
 import ArgumentParser from '/ArgumentParser';
 import FileAnalysis from '/FileAnalysis';
-import TextFormat from '/formats/TextFormat';
-import JSONFormat from '/formats/JSONFormat';
+import ReportWriter from '/ReportWriter';
 
 export default function() {
     const fileOperations = FileOperations();
@@ -16,33 +15,17 @@ export default function() {
         return result;
     }
 
-    function selectOutputStream(path) {
-        if (path === undefined) {
-            return process.stdout;
-        }
-        return fs.createWriteStream(path);
-    }
-
-    function selectFormat(path) {
-        if (path === undefined) {
-            return TextFormat();
-        }
-        return JSONFormat();
-    }
-
     const self = {};
 
     self.run = function(argv) {
         var options = parser.parse(argv);
-        var outputStream = selectOutputStream(options.output);
-        var prettier = selectFormat(options.output);
-        var whitelist = readWhitelist(options.whitelist);
+        var writer = ReportWriter(options.output);
 
+        var whitelist = readWhitelist(options.whitelist);
         var analysis = FileAnalysis(whitelist);
 
         var reports = analysis.processFiles(options.args);
-        var output = prettier.format(reports);
-        outputStream.write(output);
+        writer.write(reports);
     };
 
     return self;
---
+++ src/ReportWriter.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import TextFormat from '/formats/TextFormat';
+import JSONFormat from '/formats/JSONFormat';
+
+export default function(path) {
+    function selectOutputStream() {
+        if (path === undefined) {
+            return process.stdout;
+        }
+        return fs.createWriteStream(path);
+    }
+
+    function selectFormat() {
+        if (path === undefined) {
+            return TextFormat();
+        }
+        return JSONFormat();
+    }
+
+    const outputStream = selectOutputStream();
+    const prettier = selectFormat();
+
+    const self = {};
+
+    self.write = function(reports) {
+        var output = prettier.format(reports);
+        outputStream.write(output);
+    };
+
+    return self;
+};
