49960d679a792b00fe10b4e3978c4564525f9482
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-01-18 11:26:57 +0100

    [clean] Removed some vars

--- test/unit/ArgumentParser.spec.js
+++ test/unit/ArgumentParser.spec.js
@@ -1,8 +1,8 @@
 import ArgumentParser from '/ArgumentParser';
 
 describe('ArgumentParser', function() {
-    var subject;
-    var helpMessage = '\n\
+    let subject;
+    const helpMessage = '\n\
   Usage: programName [options] <css_file_paths>\n\
 \n\
   Checks constraints on css files\n\
@@ -21,27 +21,27 @@ describe('ArgumentParser', function() {
 
     describe('parser', function() {
         it('should extract the 3rd argument as the input file', function() {
-            var options = subject.parse(['node', 'programName', 'inputFile']);
+            const options = subject.parse(['node', 'programName', 'inputFile']);
             assert.equal('inputFile', options.args);
         });
 
         it('should return an array with the input file', function() {
-            var options = subject.parse(['node', 'programName', 'inputFile']);
+            const options = subject.parse(['node', 'programName', 'inputFile']);
             assert.isArray(options.args);
         });
 
         it('should return the configuration file with optional argument -w', function() {
-            var options = subject.parse(['node', 'programName', '-w', 'whitelist.json', 'inputFile']);
+            const options = subject.parse(['node', 'programName', '-w', 'whitelist.json', 'inputFile']);
             assert.equal('whitelist.json', options.whitelist);
         });
         
         it('should accept long option --whitelist', function() {
-            var options = subject.parse(['node', 'programName', '--whitelist', 'whitelist.json', 'inputFile']);
+            const options = subject.parse(['node', 'programName', '--whitelist', 'whitelist.json', 'inputFile']);
             assert.equal('whitelist.json', options.whitelist);
         });
 
         it('should extract the input file correctly', function() {
-            var options = subject.parse(['node', 'programName', '-w', 'whitelist.json', 'inputFile']);
+            const options = subject.parse(['node', 'programName', '-w', 'whitelist.json', 'inputFile']);
             assert.equal('inputFile', options.args);
         });
 
@@ -70,7 +70,7 @@ describe('ArgumentParser', function() {
         });
 
         it('should accept option --output to specify ouput file', function() {
-            var options = subject.parse(['_', '_', '--output', 'output.json', 'input.css']);
+            const options = subject.parse(['_', '_', '--output', 'output.json', 'input.css']);
             assert.equal('output.json', options.output);
         });
     });
