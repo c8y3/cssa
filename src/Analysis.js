7bcad7a83119b75bce697c9a0e3175798e68d069
c8y3 <25362953+c8y3@users.noreply.github.com>
2018-03-01 15:07:08 +0100

    [clean] Following eslint

--- src/Analysis.js
+++ src/Analysis.js
@@ -2,9 +2,8 @@ import css from 'css';
 import TreeTraversal from '/TreeTraversal';
 
 export default function(whitelist) {
-
     function parse(input, path) {
-        const ast = css.parse(input, { source: path });
+        const ast = css.parse(input, {source: path});
         return TreeTraversal(ast);
     }
 
@@ -13,7 +12,7 @@ export default function(whitelist) {
     self.process = function(path, input) {
         const ast = parse(input, path);
         const summary = ast.traverse();
-        
+
         summary.remove(whitelist);
         return {
             path: path,
